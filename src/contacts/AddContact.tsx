936639a5b4e1c198b53d59a28013cccd250fdfa8
Oltion Zefi <oltionzefi@gmail.com>
2019-12-21 19:57:03 +0100

    Boilerplate: Adding Container as HOC

--- src/contacts/AddContact.tsx
+++ src/contacts/AddContact.tsx
@@ -3,7 +3,7 @@ import { WithTranslation, withTranslation } from 'react-i18next';
 
 const AddContactComponent = (props: WithTranslation) => {
 	const { t } = props;
-	return <div>{t('contacts.add_contact.placeholder')}</div>;
+	return <div className="AddContacts">{t('contacts.contacts.add-contact')}</div>;
 };
 
 export const AddContact = withTranslation()(AddContactComponent);

dad407862af130a06b86882ee8c3b96aa47ff929
Oltion Zefi <oltionzefi@gmail.com>
2020-01-08 23:40:06 +0100

    Boilerplate: Jest Configuration

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+		'^.+\\.ts?$': 'ts-jest',
+		'^.+\\.jsx?$': 'babel-jest',
+		'^.+\\.js?$': 'babel-jest'
+	},
+	roots: ['<rootDir>/src'],
+	testMatch: [
+		'<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+		'<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+	],
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	transformIgnorePatterns: ['<rootDir>/(node_modules)/'],
+	modulePathIgnorePatterns: ['<rootDir>/(node_modules)/'],
+	modulePaths: ['<rootDir>/src', '<rootDir>/node_modules'],
+	setupFiles: ['<rootDir>/src/setupTests.ts']
+};
